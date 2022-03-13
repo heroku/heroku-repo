aa651e069d5d53751c457b2a8a467f907b5fc99d
NaveenTn49 <91258682+NaveenTn49@users.noreply.github.com>
2022-03-13 17:45:59 +0530

    Create naveen.py

---
+++ naveen.py
@@ -0,0 +1,15 @@
+class Config(Config):
+    TG_BOT_TOKEN= "5148770189:AAFp4gt5CZYkvs-5Y8IfzKMB3gCqKCGJQCA" #imp
+    APP_ID = 8827454 #imp
+    API_HASH = "b8a53f89f8cab2f95e5a99360d6db455" #imp
+    AUTH_CHANNEL = [-1001657991072, 944841802] #imp replace your_id with your id from telegram or delete
+    INDEX_LINK = "https://torrentleech.torrentleech-gdrive.workers.dev
+    GLEECH_COMMAND = "gleech@TorrentLeech_Gdrivebot"
+    YTDL_COMMAND = 'ytdl@TorrentLeech_Gdrivebot'
+    TELEGRAM_LEECH_COMMAND_G = "tleech@TorrentLeech_Gdrivebot"
+    CLONE_COMMAND_G = "gclone@groupname"
+    PYTDL_COMMAND_G = "pytdl@groupname"
+    DESTINATION_FOLDER = "TorrentLeech-Gdrive"
+    LEECH_COMMAND = "leech@TorrentLeech_Gdrivebot"
+    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.-XX9fgU--","token_type":"Bearer","refresh_token":"1//0gLZEp8-VV2rZyourtCgYIARAAGBASNwF-","expiry":"2020-09-03T14:22:34.599776393Z"}\nteam_drive = """
+    #put your config(replacing new lines with \n) in triple quote like above: """<your one liner config>"""
